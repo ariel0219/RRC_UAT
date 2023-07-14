f5ba426d5d3eb4730a7fe7dff613aac0e4b59ae3
Zhang <xiaojing.zhang@cgi.com>
2023-07-14 13:47:10 -0500

    add description in string in IETRS approval toast

--- force-app/main/default/aura/IETRS_InspApproveAction/IETRS_InspApproveActionController.js
+++ force-app/main/default/aura/IETRS_InspApproveAction/IETRS_InspApproveActionController.js
@@ -8,7 +8,7 @@
         action.setCallback(this, function(response){
             let state = response.getState();
             if(state === 'SUCCESS') {
-                h.showToast('success', 'Approval Process Started', 'The approval process is running now. You will receive an email if there are any errors.');      
+                h.showToast('success', 'Approval Process Started', 'The approval process is running now. You will receive an email if there are any errors. Please wait a moment and refresh page to see updated result. This may take a moment');      
                 cmp.destroy();
                 $A.get('e.force:closeQuickAction').fire();
             } else {
